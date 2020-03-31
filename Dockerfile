a87e15bd6864d16d37f89b52c900bf230991e779
Renna <renna@reemet.me>
2020-03-31 13:17:17 +0100

    Sync

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:10.16.0
+
+RUN mkdir /app
+WORKDIR /app
+
+EXPOSE 8080
---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+router.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+router.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+router.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+export default router;
